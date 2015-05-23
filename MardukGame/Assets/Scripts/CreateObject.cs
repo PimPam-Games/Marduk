8695ca0528d109f6f4017fc5552be4339c71c02f
Agustin Bauer <agustinbauer1@gmail.com>
2015-05-23 15:27:15 -0300

    flamethrower implementation improved

---
+++ MardukGame/Assets/Scripts/CreateObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateObject : MonoBehaviour {
+
+	public GameObject objectToCreate;
+	public bool loop;
+	public float delay;
+	public Direction rotation;
+	Quaternion rot;
+	public enum Direction{Right, Left, Up ,Down};
+	// Use this for initialization
+	void Start () {
+
+		rot = Quaternion.identity;
+		switch (rotation)
+		{
+		case Direction.Down:
+			rot.eulerAngles = new Vector3(90, 180, 0);
+			break;
+		case Direction.Up:
+			rot.eulerAngles = new Vector3(-90, 0, 0);
+			break;
+		case Direction.Right:
+			rot.eulerAngles = new Vector3(0, 90, 0);
+			break;
+		case Direction.Left:
+			rot.eulerAngles = new Vector3(0, 270, 0);
+			break;
+		default:
+			rot.eulerAngles = new Vector3(-90, 0, 0);
+			break;
+		}
+		StartCoroutine (InstantiateObject());
+	}
+
+	IEnumerator InstantiateObject () {
+		yield return new WaitForSeconds (delay);
+	    Instantiate (objectToCreate, transform.position, rot);
+		while (loop) {
+			yield return new WaitForSeconds (delay);
+			Instantiate (objectToCreate, transform.position, rot);
+		}
+	}
+
+}
--- MardukGame/Assets/Scripts/ParticleCollision.cs
+++ MardukGame/Assets/Scripts/ParticleCollision.cs
@@ -5,14 +5,56 @@ public class ParticleCollision : MonoBehaviour {
 
 	public Types.Element elem = Types.Element.Fire ;
 	public float dmg = 5f;
+	public BoxCollider2D col;
+	private string direction;
+
 	// Use this for initialization
 	void Start () {
-	
+		//Debug.Log (transform.rotation.eulerAngles);
+		if (transform.rotation.eulerAngles.x == 270)
+			direction = "Up";
+		if(transform.rotation.eulerAngles.x == 90)
+			direction = "Down";
+		if(transform.rotation.eulerAngles.y == 90)
+			direction = "Right";
+		if(transform.rotation.eulerAngles.y == 270)
+			direction = "Left";
+		transform.rotation = Quaternion.Euler (0,0,0);
+		StartCoroutine (RemoveColAfterTime(1.7f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (direction == "Left") { //agranda el collider dinamicamente hacia la izquierda
+			if(col.size.x < 20 ){
+				col.size += new Vector2(0.6f,0);
+				col.offset += new Vector2 (-0.25f,0);
+			}
+		}
+		if (direction == "Right") {
+			if(col.size.x < 20 ){
+				col.size += new Vector2(0.6f,0);
+				col.offset += new Vector2 (0.25f,0);
+			}
+		}
+		if (direction == "Up") {
+			if(col.size.y < 20 ){
+				col.size += new Vector2(0,0.6f);
+				col.offset += new Vector2 (0,0.25f);
+			}
+		}
+		if (direction == "Down") {
+			if(col.size.y < 20 ){
+				col.size += new Vector2(0,0.6f);
+				col.offset += new Vector2 (0,-0.25f);
+			}
+		}
+	}
+
+	IEnumerator RemoveColAfterTime(float collLifetime) {
+		yield return new WaitForSeconds (collLifetime);
+		col.enabled = false;
+		
 	}
 
 	void OnTriggerEnter2D (Collider2D col){
