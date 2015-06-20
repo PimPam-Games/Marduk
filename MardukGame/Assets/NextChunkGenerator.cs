05d19f62ff048d82c96830058cc374c5c899ec33
Luciano Putruele <lputruele@gmail.com>
2015-06-20 19:13:42 -0300

    Procedural level generation update

--- MardukGame/Assets/NextChunkGenerator.cs
+++ MardukGame/Assets/NextChunkGenerator.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using g = GameController;
+using System.Collections.Generic;
 
 public class NextChunkGenerator : MonoBehaviour {
 
 
-	public GameObject nextChunk;
+	public GameObject[] chunkPool;
+	public List<Transform> enemies;
 	public Transform chunkStart;
 	public Transform chunkEnd;
+	private bool alreadyGenerated = false;
 	// Use this for initialization
 	void Start () {
-	
+		foreach(Transform enemyPos in enemies){
+			int index = Random.Range(0,g.enemyList.Length); //slecciona un enemigo aleatorio de la lista de enemigos
+			GameObject newEnemy = (GameObject)Instantiate (g.enemyList[index],enemyPos.position,enemyPos.rotation);
+			DontDestroyOnLoad(newEnemy);
+			//enemies.Add(newEnemy);
+		}
+		//Debug.Log ("meto la key: " + g.currLevelName);
+		//g.enemiesPerLevel.Add (g.currLevelName, enemies);		
 	}
 	
 	// Update is called once per frame
@@ -18,9 +29,10 @@ public class NextChunkGenerator : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
-	
-		if (col.gameObject.tag == "Player") {
-			Instantiate(nextChunk,chunkEnd.position,chunkEnd.rotation);
+		int r = Random.Range (0,chunkPool.Length);
+		if (col.gameObject.tag == "Player" && !alreadyGenerated) {
+			alreadyGenerated = true;
+			Instantiate(chunkPool[r],chunkEnd.position,chunkEnd.rotation);
 			Debug.Log ("Generar Chunk");
 		}
 	}
