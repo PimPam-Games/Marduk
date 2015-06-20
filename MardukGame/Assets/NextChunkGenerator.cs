6b50158fc9f6ab2f2462289f2b24ab7a44e4f257
Agustin Bauer <agustinbauer1@gmail.com>
2015-06-20 15:52:46 -0300

    Chunks

---
+++ MardukGame/Assets/NextChunkGenerator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextChunkGenerator : MonoBehaviour {
+
+
+	public GameObject nextChunk;
+	public Transform chunkStart;
+	public Transform chunkEnd;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+	
+		if (col.gameObject.tag == "Player") {
+			Instantiate(nextChunk,chunkEnd.position,chunkEnd.rotation);
+			Debug.Log ("Generar Chunk");
+		}
+	}
+}
