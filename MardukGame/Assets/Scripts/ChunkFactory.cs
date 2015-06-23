7940a8debad0f4614b2269b7998ab52cdd9e08c2
Agustin Bauer <agustinbauer1@gmail.com>
2015-06-23 16:35:43 -0300

    Dinamic Background in progress

--- MardukGame/Assets/Scripts/ChunkFactory.cs
+++ MardukGame/Assets/Scripts/ChunkFactory.cs
@@ -5,14 +5,20 @@ using System.Collections.Generic;
 public class ChunkFactory : MonoBehaviour {
 
 	public int zone = 1;
+	public string bgName = "Mountain";
+
 	private static Object[] chunkPool;
 	private static List<Object> commonChunks = new List<Object>();
 	private static List<Object> castleChunks = new List<Object>();
+	private static Object bg;
 	private static bool isEntry = false;
+	public static float bgCount = 0;
+	public static float bgPerChunk = 7; //cada cuantos chunks debe generar el fondo
 
 	// Use this for initialization
 	void Awake () {
 		chunkPool = Resources.LoadAll("Level/ChunksZone" + zone, typeof(Object));
+		bg = Resources.Load ("Level/Background/" + bgName);
 		for (int i = 0; i< chunkPool.Length; i++) {
 			if(chunkPool[i].name.Contains("Castle") && !chunkPool[i].name.Contains("Entry")){
 				castleChunks.Add(chunkPool[i]);
@@ -29,6 +35,7 @@ public class ChunkFactory : MonoBehaviour {
 	}
 
 	public static void GenerateChunk(Vector3 pos, Quaternion rot){
+		bgCount++;
 		if (isEntry) {
 			int r = Random.Range (0,castleChunks.Count);
 			GameObject newChunk = (GameObject)Instantiate (castleChunks [r], pos, rot);
@@ -39,6 +46,11 @@ public class ChunkFactory : MonoBehaviour {
 			GameObject newChunk = (GameObject)Instantiate (commonChunks [r], pos, rot);
 			if(newChunk.name.Contains("Entry"))
 				isEntry = true;
+		}
+		if (bgCount >= bgPerChunk) {
+			Debug.Log("Cree un bg");
+			bgCount = 0;
+			Instantiate(bg,new Vector3(pos.x + 35,pos.y - 2,35),rot);
 		}
 	}
 }
---
+++ MardukGame/Assets/Scripts/UI/InputControllerGui.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputControllerGui : MonoBehaviour {
+	
+	private MyGUI gui;
+	private bool gamePaused = false;
+	// Use this for initialization
+	void Awake () {
+		gui = GetComponent<MyGUI>();
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("ToggleInventory")) {
+			gui.ToggleInventoryWindow();
+			SetMouseVisible();
+		}
+		if (Input.GetButtonUp ("ToggleCharacterWindow")) {
+			gui.ToggleCharacterWindow();
+			SetMouseVisible();
+		}
+		if(Input.GetButtonUp ("Save")){
+			Persistence.Save();
+			Debug.Log("Save Data");
+		}
+		if (Input.GetButtonUp ("Load")) {
+			Persistence.Load();
+			Debug.Log("Load Data");
+		}
+		if (Input.GetButtonUp ("Escape")) {
+			Debug.Log("Pause");
+			if(!gamePaused){
+				Time.timeScale = 0;
+				gamePaused = true;
+			}
+			else{
+				Time.timeScale = 1.0f;
+				gamePaused = false;
+			}
+		}
+	}
+
+	private void SetMouseVisible(){
+		if(MyGUI.InventoryOpen() || MyGUI.CharacterWindowOpen())
+			Cursor.visible = true;
+		else
+			Cursor.visible = false;
+	}
+}
