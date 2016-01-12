2b8672986331a93e5e337ebe44e9868842ce0e0c
Agustin Bauer <agustinbauer1@gmail.com>
2016-01-12 04:01:18 -0300

    Default screen resolution and graphics quality

---
+++ MardukGame/Assets/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+	public Image mainMenu;
+
+	public void Back(){
+		StartCoroutine(BackPush());
+	}
+	
+	IEnumerator BackPush(){
+		yield return new WaitForSeconds (0.3f); //ahce que espere un poco para que se escuche el sonido
+		mainMenu.gameObject.SetActive(true);
+		this.gameObject.SetActive(false);
+	}
+}
--- MardukGame/Assets/Scripts/UI/MainMenu.cs
+++ MardukGame/Assets/Scripts/UI/MainMenu.cs
@@ -5,12 +5,34 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour {
 
 	public Image playMenuImage;
-
+	public Image optionsMenu;
 	void Awake(){
+		//Debug.Log( Screen.currentResolution.);
+		bool resolutionOk = false;
+		/*resoluciones de pantalla compatibles con el juego*/
+		/*if(Screen.currentResolution.width == 1280 &&  Screen.currentResolution.height == 720) 
+			resolutionOk = true;
+		if(Screen.currentResolution.width == 1360 &&  Screen.currentResolution.height == 768)
+			resolutionOk = true;
+		if(Screen.currentResolution.width == 1366 &&  Screen.currentResolution.height == 768)
+			resolutionOk = true;
+		if(Screen.currentResolution.width == 1600 &&  Screen.currentResolution.height == 900)
+			resolutionOk = true;*/
+		Screen.SetResolution(1600,900,true);
+		//auxtext.text = "res " + Screen.currentResolution.width.ToString() + " x " + Screen.currentResolution.height.ToString();
+	/*	if(!resolutionOk){
+			Screen.SetResolution(1280,720,true);
+		}*/
+		
+		//QualitySettings.SetQualityLevel(5); //por defecto se setea la calidad de grafricos mas alta
 	}
 	
 	public void NewGame(){
 		StartCoroutine(ButtonPush(1));
+	}
+
+	public void Options(){
+		StartCoroutine(ButtonPush(2));
 	}
 
 	public void ExitGame(){
@@ -21,6 +43,10 @@ public class MainMenu : MonoBehaviour {
 		yield return new WaitForSeconds (0.3f);
 		if(btnId == 1){
 			playMenuImage.gameObject.SetActive(true);
+			this.gameObject.SetActive(false);
+		}
+		if(btnId == 2){
+			optionsMenu.gameObject.SetActive(true);
 			this.gameObject.SetActive(false);
 		}
 		if(btnId == 3){
